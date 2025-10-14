082a4e20814299ac25fdcdec0085a00a57ab560a
parth <parthsingla5007@gmail.com>
2025-10-14 23:10:59 +0530

    Initializing game

--- app.js
+++ app.js
@@ -44,3 +44,50 @@ function updateScores() {
   currScoreTag.innerText = currScore;
   maxScoreTag.innerText = maxScore;
 }
+
+
+
+
+function initGame(e) {
+  let key = e.target.value.toLowerCase();
+  if (
+    key.match(/^[A-Za-z]+$/) &&
+    !incorrectLetters.includes(` ${key}`) &&
+    !correctLetters.includes(key)
+  ) {
+    if (word.includes(key)) {
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] == key) {
+          correctLetters += key;
+          inputs.querySelectorAll("input")[i].value = key;
+        }
+      }
+    } else {
+      maxGuesses--;
+      incorrectLetters.push(` ${key}`);
+    }
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+  }
+  typingInput.value = "";
+
+  setTimeout(() => {
+    if (correctLetters.length === word.length) {
+      currScore += 10; // +10 per word correct
+      if (currScore > maxScore) maxScore = currScore;
+      updateScores();
+
+      alert(`🎉 Congrats! You found the word ${word.toUpperCase()}`);
+      randomWord();
+    } else if (maxGuesses < 1) {
+      alert(`💀 Game over! The word was ${word.toUpperCase()}`);
+      for (let i = 0; i < word.length; i++) {
+        inputs.querySelectorAll("input")[i].value = word[i];
+      }
+
+      currScore = 0; // reset score on failure
+      updateScores();
+    }
+  }, 100);
+}
+
